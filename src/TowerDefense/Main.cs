9452fe1674ab50f68d30303c28557d8b28022102
Philip Proplesch <philip@proplesch.de>
2011-10-06 16:30:07 +0200

    Initial commit

---
+++ src/TowerDefense/Main.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TowerDefense.Models;
+
+namespace TowerDefense
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            Spielfeld spielfeld = new Spielfeld();
+
+            Weg weg = new Weg();
+            spielfeld.WegAbschnitte.Add(weg);            
+        }
+    }
+}
---
+++ src/TowerDefense/Models/Spielfeld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TowerDefense.Models
+{
+    public class Spielfeld
+    {        
+        public List<Weg> WegAbschnitte { get; set; }
+
+        public Spielfeld()
+        {
+            WegAbschnitte = new List<Weg>();
+        }
+    }
+}
